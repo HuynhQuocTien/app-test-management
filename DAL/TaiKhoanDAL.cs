d016653e0dec264777b410a3dc0709bd577d42a1
Huynh Quoc Tien <quoctien01062003@gmail.com>
2024-10-15 17:24:28 +0700

    merge An

--- DAL/TaiKhoanDAL.cs
+++ DAL/TaiKhoanDAL.cs
@@ -1,4 +1,4 @@
-using DTO;
+﻿using DTO;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
@@ -10,6 +10,99 @@ namespace DAL
         public static TaiKhoanDAL getInstance()
         {
             return new TaiKhoanDAL();
+        }
+        // kết nối csdl
+        private SqlConnection conn;
+        public TaiKhoanDAL()
+        {
+            conn = GetConnectionDb.GetConnection(); // Khởi tạo đối tượng ConnectDB
+        }
+        // Kiểm tra gmail có trong db không
+        public bool kiemTraEmailNguoiDung(string email)
+        {
+            string query = $"SELECT COUNT(*) FROM NguoiDung WHERE Email = @email";
+            try
+            {
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@email", email);
+                    // kiểu var giống biến trong js
+                    var result = command.ExecuteScalar();
+                    return (int)result > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        public bool kiemTraTaiKhoan(string taiKhoan, string matKhau)
+        {
+            string query = $"SELECT Password FROM NguoiDung WHERE MaNguoiDung = @TaiKhoan";
+            try
+            {
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    // thêm tham số vào câu lệnh SQL
+                    command.Parameters.AddWithValue("@TaiKhoan", taiKhoan);
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read()) // Kiểm tra xem có hàng nào không
+                        {
+                            string matKhauNguoiDung = reader["Password"].ToString(); // Lấy giá trị từ cột Password
+                            Console.Write(matKhauNguoiDung);
+                            return matKhauNguoiDung.Equals(matKhau);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Tài khoản không tồn tại.");
+                            return false;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                // Đảm bảo rằng kết nối được đóng
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
+
+        }
+        public bool suaMatKhauNguoiDung(string email, string matKhau)
+        {
+            string query = "UPDATE NguoiDung SET Password = @MatKhau WHERE Email = @Email";
+            try
+            {
+                using (SqlCommand command = new SqlCommand(query, conn))
+                {
+                    command.Parameters.AddWithValue("@MatKhau", matKhau);
+                    command.Parameters.AddWithValue("@Email", email);
+                    int rowsAffected = command.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                if (conn.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+            }
         }
 
         public bool Add(TaiKhoanDTO taiKhoan)
