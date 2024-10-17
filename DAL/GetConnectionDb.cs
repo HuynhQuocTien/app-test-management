a622419b2b64bd3fb72d36973e8419640d396eee
Huynh Quoc Tien <quoctien01062003@gmail.com>
2024-10-17 10:02:23 +0700

    up dll

--- DAL/GetConnectionDb.cs
+++ DAL/GetConnectionDb.cs
@@ -12,8 +12,8 @@ namespace DAL
     {
         public static SqlConnection GetConnection()
         {
-            //string connectionsString = "Data Source=localhost;Initial Catalog=app-test-management;User ID=sa;Password=123456;Integrated Security=True";
-            string connectionsString = "Data Source=LAPTOP-3M6UG0D2\\SQLEXPRESS;Initial Catalog=app-test-management;Integrated Security=True;";
+            string connectionsString = "Data Source=localhost;Initial Catalog=Test_Management_Db;User ID=sa;Password=123456;Integrated Security=True";
+            //string connectionsString = "Data Source=LAPTOP-3M6UG0D2\\SQLEXPRESS;Initial Catalog=app-test-management;Integrated Security=True;";
             SqlConnection sqlConn = new SqlConnection(connectionsString);
             if (sqlConn.State == System.Data.ConnectionState.Closed)
             {
