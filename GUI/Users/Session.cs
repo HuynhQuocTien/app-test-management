9d496e938a1e61d7a56625634976d5493f5c043b
htuan2800 <huynhngoctuan48@gmail.com>
2024-10-18 14:19:48 +0700

    .

---
+++ GUI/Users/Session.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Users
+{
+    public static class Session
+    {
+        // Biến tĩnh để lưu thông tin UserID
+        public static string UserID { get; set; }
+    }
+}
