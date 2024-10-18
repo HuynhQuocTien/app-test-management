368cd5258e049673b2d4ff866537a5ffb155edf5
htuan2800 <huynhngoctuan48@gmail.com>
2024-10-18 14:53:08 +0700

    xem PC

--- GUI/PhanCong/PhanCongControl.cs
+++ GUI/PhanCong/PhanCongControl.cs
@@ -1,4 +1,6 @@
-﻿using GUI.MonHoc;
+﻿using DAL;
+using DocumentFormat.OpenXml.Spreadsheet;
+using GUI.MonHoc;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -7,6 +9,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace GUI.PhanCong
@@ -16,6 +20,25 @@ namespace GUI.PhanCong
         public PhanCongControl()
         {
             InitializeComponent();
+            using (SqlConnection conn = GetConnectionDb.GetConnection())
+            {
+                try
+                {
+                    string query = "SELECT PC.MaPhanCong as 'Mã Phân Công', MH.TenMonHoc as 'Tên Môn Học', ND.Ten as 'Tên Giáo Viên' FROM PhanCong PC INNER JOIN MonHoc MH ON MH.MaMonHoc = PC.MaMonHoc INNER JOIN NguoiDung ND ON  ND.MaNguoiDung=PC.MaGV;";
+                    SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                    DataTable dt = new DataTable();
+
+                    // Đổ dữ liệu vào DataTable
+                    da.Fill(dt);
+
+                    // Gán dữ liệu vào DataGridView
+                    dataGridView1.DataSource = dt;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Đã có lỗi xảy ra: " + ex.Message);
+                }
+            }
         }
 
         private void btnThem_Click(object sender, EventArgs e)
--- app-test-management/obj/Debug/net8.0-windows/app-test-management.AssemblyInfo.cs
+++ app-test-management/obj/Debug/net8.0-windows/app-test-management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app-test-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+274056a82bf930864e7deac34e38b3893e61727d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d496e938a1e61d7a56625634976d5493f5c043b")]
 [assembly: System.Reflection.AssemblyProductAttribute("app-test-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app-test-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
