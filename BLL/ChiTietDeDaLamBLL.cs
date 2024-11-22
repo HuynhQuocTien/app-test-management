11d6756d90cbf86926ef5b22b36ca1e8db38bbad
HUỲNH QUỐC TIẾN <115643869+HuynhQuocTien@users.noreply.github.com>
2024-11-23 03:42:55 +0700

    update (ChiTietDeDaLam): DTO,BLL,DAL

---
+++ BLL/ChiTietDeDaLamBLL.cs
@@ -0,0 +1,82 @@
+﻿using DAL;
+using DTO;
+using System;
+using System.Collections.Generic;
+
+namespace BLL
+{
+    public class ChiTietDeDaLamBLL
+    {
+        private ChiTietDeDaLamDAL _chiTietDeDaLamDAL;
+
+        public ChiTietDeDaLamBLL()
+        {
+            _chiTietDeDaLamDAL = ChiTietDeDaLamDAL.getInstance();
+        }
+
+        public bool AddChiTietDeDaLam(ChiTietDeDaLamDTO chiTietDeDaLam)
+        {
+            try
+            {
+                return _chiTietDeDaLamDAL.Add(chiTietDeDaLam);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in AddChiTietDeDaLam: " + ex.Message);
+                return false;
+            }
+        }
+
+        public bool DeleteChiTietDeDaLam(ChiTietDeDaLamDTO chiTietDeDaLam)
+        {
+            try
+            {
+                return _chiTietDeDaLamDAL.Delete(chiTietDeDaLam);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in DeleteChiTietDeDaLam: " + ex.Message);
+                return false;
+            }
+        }
+
+        public bool UpdateChiTietDeDaLam(ChiTietDeDaLamDTO chiTietDeDaLam)
+        {
+            try
+            {
+                return _chiTietDeDaLamDAL.Update(chiTietDeDaLam);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in UpdateChiTietDeDaLam: " + ex.Message);
+                return false;
+            }
+        }
+
+        public List<ChiTietDeDaLamDTO> GetAllChiTietDeDaLam()
+        {
+            try
+            {
+                return _chiTietDeDaLamDAL.GetAll();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in GetAllChiTietDeDaLam: " + ex.Message);
+                return new List<ChiTietDeDaLamDTO>();
+            }
+        }
+
+        public ChiTietDeDaLamDTO GetChiTietDeDaLamById(ChiTietDeDaLamDTO chiTietDeDaLam)
+        {
+            try
+            {
+                return _chiTietDeDaLamDAL.GetById(chiTietDeDaLam);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error in GetChiTietDeDaLamById: " + ex.Message);
+                return null;
+            }
+        }
+    }
+}
--- DTO/ChiTietDeDaLamDTO.cs
+++ DTO/ChiTietDeDaLamDTO.cs
@@ -8,13 +8,18 @@ namespace DTO
 {
     public class ChiTietDeDaLamDTO
     {
+        public int MaChiTietDeDaLam { get; set; }
         public int MaDe { get; set; }
         public int MaCauHoi { get; set; }
+        public int MaKetQua { get; set; }
         public ChiTietDeDaLamDTO() { }
-        public ChiTietDeDaLamDTO(int maDe, int maCauHoi)
+
+        public ChiTietDeDaLamDTO(int maChiTietDeDaLam, int maDe, int maCauHoi, int maKetQua)
         {
+            MaChiTietDeDaLam = maChiTietDeDaLam;
             MaDe = maDe;
             MaCauHoi = maCauHoi;
+            MaKetQua = maKetQua;
         }
     }
 }
