3fb7168137e0870c199170ed80d0e35b8004d56f
thuan <thuanann.sama@gmail.com>
2024-11-27 03:09:43 +0700

    new run

--- app-test-management/obj/Debug/net8.0-windows/app-test-management.AssemblyInfo.cs
+++ app-test-management/obj/Debug/net8.0-windows/app-test-management.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app-test-management")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a91d6c1f9f55bc02c27625a34a47507fc881d8a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a58e71cc595f2c68edc349d1241a25e33637be")]
 [assembly: System.Reflection.AssemblyProductAttribute("app-test-management")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app-test-management")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
